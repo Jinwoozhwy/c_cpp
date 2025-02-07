a46d32d929273e776097b582f75c66b65fd6fe39
jinwooyu <zhangzhenyu678@outlook.com>
2025-02-07 22:21:46 +0800

    2025_02_07

--- test_25_01_04/test_25_01_04/test.c
+++ test_25_01_04/test_25_01_04/test.c
@@ -1,3 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
+#include "add.h"
+//µ¼Èë¾²Ì¬¿â
+#pragma comment(lib, "add.lib")
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	int ret = Add(a, b);
+	printf("%d\n", ret);
+
+	return 0;
+}
